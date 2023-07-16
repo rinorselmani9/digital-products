a8d875014f7f57e56b21b9647f47387bd752f4ef
rinorselmani9 <rs44583@ubt-uni.net>
2023-07-16 13:01:17 +0200

    home page finished with styling and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image"
+import styles from "./page.module.css"
+import Hero from 'public/hero.png'
+
 export default function Home() {
-  return (  
-    <div>
-      <h1>Home</h1>
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital products</h1>
+        <p className={styles.description}>Turning your ideas on to reality</p>
+        <button className={styles.button}>See our works </button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt='' className={styles.img} />
+      </div>
     </div>
   )
 }
