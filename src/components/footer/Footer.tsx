6d1cfb1092735cc6366a8058d3197d296fa6d95e
rinorselmani9 <rs44583@ubt-uni.net>
2023-07-16 12:44:10 +0200

    social icons on footer, some styling changes

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>Digital Products</div>
-      <div></div>
+      <div className={styles.socials}>
+        <Image src='/1.png' width={15} height={15} className={styles.icon} alt='Digital Products'/>
+        <Image src='/2.png' width={15} height={15} className={styles.icon} alt='Digital Products'/>
+        <Image src='/3.png' width={15} height={15} className={styles.icon} alt='Digital Products'/>
+        <Image src='/4.png' width={15} height={15} className={styles.icon} alt='Digital Products'/>
+      </div>
     </div>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import styles from './navbar.module.css'
 
 const links = [
   {
@@ -34,15 +35,15 @@ const links = [
     url:'/dashboard',
   },
 ]
-
 const Navbar = () => {
   return (
-    <div>
-      <Link href={'/'}>Digital Products</Link>
-      <div>
+    <div className={styles.container}>
+      <Link href={'/'} className={styles.logo}>Digital Products</Link>
+      <div className={styles.links}>
         {links.map(link => (
-          <Link key={link.id} href={link.url}>{link.title}</Link>
+          <Link key={link.id} href={link.url} >{link.title}</Link>
         ))}
+        <button className={styles.logout}>Logout</button>
       </div>
     </div>
   )
