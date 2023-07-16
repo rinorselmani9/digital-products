37fc0e193f465cd4d1b5c29553808a187587853e
rinorselmani9 <rs44583@ubt-uni.net>
2023-07-16 13:34:41 +0200

    contact page done with styling and content, added animation on home page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from "react"
+import styles from "./page.module.css"
+import Image from "next/image"
+import Link from "next/link"
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let's keep in touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src={"/contact.png"} alt='' fill={true} className={styles.image} />
+        </div>
+        <form className={styles.form}>
+          <input type='text' placeholder='name' className={styles.input} />
+          <input type='text' placeholder='email' className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder='message'
+            cols={30}
+            rows={10}
+          ></textarea>
+          <Link href={"#"}>
+            <button className={styles.button}>Send</button>
+          </Link>
+        </form>
+      </div>
+    </div>
   )
 }
 
